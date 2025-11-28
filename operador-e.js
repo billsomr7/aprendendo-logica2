51b60c916eb5f75f6a8aec0f664af75c15960445
billsomr7 <billsomr7@gmail.com>
2025-11-28 10:38:32 -0300

    Add files via upload

---
+++ operador-e.js
@@ -0,0 +1,39 @@
+/* let maiorIdade = true
+let temIngresso = true
+
+if (maiorIdade && temIngresso) {
+    alert("Pode entrar no show!")
+} else {
+    alert("Não pode entrar no show!")
+ } */
+
+   /*  let maiorIdade = true
+    let temIngresso = false 
+
+    if (maiorIdade && temIngresso) {
+        alert("Pode entrar no show!")
+    } else {
+        alert("Não pode entrar no show!")
+     } */
+
+        /* 
+        let primeiraPessoa = 22
+        let segundaPessoa = 22
+        if (primeiraPessoa > segundaPessoa) {
+            alert("Primeira pessoa é mais velha")
+
+        } else if (segundaPessoa > primeiraPessoa ) {
+            alert("Segunda pessoa é mais velha")
+       
+        } else {
+            alert("As duas pessoas tem a mesma idade")
+        } */
+
+        let idade = Number(prompt("Qual sua idade?"))
+
+        if (idade >= 18) {
+            alert("pode tirar CNH")
+        } else {
+            alert("não pode tirar CNH")
+        }
+
---
+++ operadorOU.js
@@ -0,0 +1,17 @@
+/* let temIngresso = true
+let nomeNaLista = true
+
+if (temIngresso || nomeNaLista) {
+    alert("Pode entrar no show!")
+} else {
+    alert("Não pode entrar no show!")
+ } */
+
+    let temIngresso = false
+    let nomeNaLista = false 
+                 /* OU */
+    if (temIngresso || nomeNaLista) {
+        alert("Pode entrar no show!")
+    } else {
+        alert("Não pode entrar no show!")
+     }
